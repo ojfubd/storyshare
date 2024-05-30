5b7ec9d96ba1b35871ad092ede57fe7f7d57342a
岡室考紀 <okmurokouki@okashitsukoukinoMacBook-Pro.local>
2024-05-30 13:53:06 +0900

    530storylogout

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
