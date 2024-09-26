7365a728813a44a0bc743c5cb9fe712cccb6a3fb
岡室考紀 <okmurokouki@okashitsukoukinoMacBook-Pro.local>
2024-09-26 13:56:58 +0900

    OGP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,70 @@
 module ApplicationHelper
+    def show_meta_tags
+      assign_meta_tags if display_meta_tags.blank?
+      display_meta_tags
+    end
+
+    def default_meta_tags
+      {
+        site: 'サイト名',
+        title: 'タイトル',
+        reverse: true,
+        separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
+        description: 'ページの説明',
+        keywords: 'ページキーワード',   #キーワードを「,」区切りで設定する
+        canonical: request.original_url,   #優先するurlを指定する
+        noindex: ! Rails.env.production?,
+        og: {
+          site_name: 'サイト名',
+          title: 'タイトル',
+          description: 'ページの説明', 
+          type: 'website',
+          url: request.original_url,
+          image: image_url('user.png'),
+          locale: 'ja_JP',
+        },
+        twitter: {
+          card: 'summary_large_image',
+          site: '@ツイッターのアカウント名',
+        },
+        fb: {
+          app_id: '自身のfacebookのapplication ID'
+        }
+      }
+    end
+
+  def assign_meta_tags(options = {})
+    defaults = default_meta_tags
+    options.reverse_merge!(defaults)
+    site = options[:site]
+    title = options[:title]
+    description = options[:description]
+    keywords = options[:keywords]
+    image = options[:image].presence || image_url('user.png')
+    configs = {
+      separator: '|',
+      reverse: true,
+      title:,
+      description:,
+      keywords:,
+      canonical: request.original_url,
+      icon: {
+        href: image_url('user.png')
+      },
+      og: {
+        type: 'website',
+        title: title.presence || site,
+        description:,
+        url: request.original_url,
+        image:,
+        site_name: site
+      },
+      twitter: {
+        site:,
+        card: 'summary_large_image',
+        image:
+      }
+    }
+    set_meta_tags(configs)
+  end
 end
